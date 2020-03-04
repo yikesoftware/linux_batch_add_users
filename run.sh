f07c37a00c756ff827c4d33e5795e33adb13e302
赤道企鹅 <40513718+yikesoftware@users.noreply.github.com>
2020-03-05 00:23:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+echo "************************************************************"
+echo "Check..."
+if [ ! -f "/data/filename" ];then
+echo "Error：./user.txt不存在！"
+exit 1
+if
+for item in `cat ./user.txt`
+do
+echo $item
+done
+echo "Prepare to add user_list in user.txt to /etc/passwd..."
 newusers < user.txt
 pwunconv
+echo "Setting passwords..."
 chpasswd < passwd.txt
 pwconv
+echo "Completed!"
+echo "************************************************************"
